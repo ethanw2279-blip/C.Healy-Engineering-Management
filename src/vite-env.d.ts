d81a3d2464e637a6bc63acd27ff82c3395c61b21
ethanw2279-blip <ethanw.2279@gmail.com>
2026-07-06 18:57:48 +0100

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL?: string
-  readonly VITE_SUPABASE_ANON_KEY?: string
+  readonly VITE_SUPABASE_URL?: https://nkkdwkwutdoazfuyjnoi.supabase.co
+  readonly VITE_SUPABASE_ANON_KEY?: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ra2R3a3d1dGRvYXpmdXlqbm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNTk4ODMsImV4cCI6MjA5ODkzNTg4M30.vwy2yNnae8x_mmuJwJ2VC5ngjT5p8mjExqzABXre1QQ
 }
 
 interface ImportMeta {
